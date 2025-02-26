460a9781cf55462d0b64b8f2fd9e9475aa50750d
ShivamTaneja <shiivamtaneja@gmail.com>
2025-02-26 17:06:21 +0530

    form (todo add, delete, update)
    Signed-off-by: ShivamTaneja <shiivamtaneja@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ const message = ref("Hello Composition API");
 const status = ref("active");
 const link = ref("https://shivamtaneja.com/");
 const tasks = ref(["Task 1", "Task 2", "Task 3"]);
+const newTask = ref("");
 
 const toggleStatus = () => {
   if (status.value === "active") {
@@ -13,6 +14,24 @@ const toggleStatus = () => {
     status.value = "inactive";
   } else {
     status.value = "active";
+  }
+};
+
+const addTask = () => {
+  if (newTask.value.trim() !== "") {
+    tasks.value.push(newTask.value);
+    newTask.value = "";
+  }
+};
+
+const deleteTask = (idx) => {
+  tasks.value.splice(idx, 1);
+};
+
+const updateTask = (idx) => {
+  if(newTask.value.trim() !== "") {
+    tasks.value[idx] = newTask.value;
+    newTask.value = "";
   }
 };
 </script>
@@ -24,9 +43,19 @@ const toggleStatus = () => {
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
 
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" v-model="newTask" />
+    <button type="submit">Submit</button>
+  </form>
+
   <h3>Taks:</h3>
   <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
+    <li v-for="(task, idx) in tasks" :key="task">
+      <span>{{ task }}</span>
+      <button @click="deleteTask(idx)">X</button>
+      <button @click="updateTask(idx)">Update</button>
+    </li>
   </ul>
 
   <a :href="link" target="_blank">Click for my webstie</a>
