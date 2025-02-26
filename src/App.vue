2cf0f17f106c07566e8f54eefb73d32cd7a43d08
ShivamTaneja <shiivamtaneja@gmail.com>
2025-02-26 14:37:04 +0530

    v-for
    Signed-off-by: ShivamTaneja <shiivamtaneja@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@ export default {
   data() {
     return {
       message: 'Hello Vue!',
-      status: 'active'
+      status: 'active',
+      tasks: ['Task one', 'Task two', 'Task three']
     }
   }
 }
@@ -15,4 +16,9 @@ export default {
   <p v-if="status === 'active'">User is active</p>
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else="status">User is inactive</p>
+
+  <h3>Taks: </h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
 </template>
