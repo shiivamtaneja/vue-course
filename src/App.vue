660dc27b8a2cf6f45602320007ec3a932175343b
ShivamTaneja <shiivamtaneja@gmail.com>
2025-02-26 17:51:32 +0530

    Hero component, passing props
    Signed-off-by: ShivamTaneja <shiivamtaneja@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
+import Hero from "@/components/Hero.vue";
 import Navbar from "@/components/Navbar.vue";
 </script>
 
 <template>
   <Navbar />
+
+  <Hero
+    title="Become a Vue Dev"
+    subTitle="Find the Vue job that fits your skills and needs"
+  />
 </template>
