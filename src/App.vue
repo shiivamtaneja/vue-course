e5780aa97e73307988d9a435e2ebb414e0917920
ShivamTaneja <shiivamtaneja@gmail.com>
2025-02-26 18:58:30 +0530

    router
    Signed-off-by: ShivamTaneja <shiivamtaneja@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
 <script setup>
-import Hero from "@/components/Hero.vue";
-import HomeCards from "@/components/HomeCards.vue";
-import JobListings from "@/components/JobListings.vue";
 import Navbar from "@/components/Navbar.vue";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
   <Navbar />
 
-  <Hero
-    title="Become a Vue Dev"
-    subTitle="Find the Vue job that fits your skills and needs"
-  />
-
-  <HomeCards />
-
-  <JobListings :limit="3" :showButton="true" />
+  <RouterView />
 </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
+import JobListings from "@/components/JobListings.vue";
+</script>
+
+<template>
+  <Hero
+    title="Become a Vue Dev"
+    subTitle="Find the Vue job that fits your skills and needs"
+  />
+
+  <HomeCards />
+
+  <JobListings :limit="3" :showButton="true" />
+</template>
